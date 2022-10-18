d5c6094f154870893b18ffc995f4d848e753e45e
Kuantize Strigoi <adrian.sunye@gmail.com>
2022-10-18 18:45:57 +0200

    created array of objects with sacrifices in variables.js

---
+++ js/variables.js
@@ -0,0 +1,282 @@
+var sacrifices = [
+    {
+        name: "Adrián",
+        surnames: {
+            surnameOne: "Suñé",
+            surnameTwo: "Mancheño"
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Albert",
+        surnames: {
+            surnameOne: "Arqués",
+            surnameTwo: ""
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Alejandra",
+        surnames: {
+            surnameOne: "Morales",
+            surnameTwo: "Cutiño"
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Alejandro",
+        surnames: {
+            surnameOne: "Buriticá",
+            surnameTwo: "Rodríguez"
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Alessandro",
+        surnames: {
+            surnameOne: "Arroyo",
+            surnameTwo: "Lebron"
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Andrés Esteban",
+        surnames: {
+            surnameOne: "Patiño",
+            surnameTwo: "Gómez"
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Camilo",
+        surnames: {
+            surnameOne: "Rocca",
+            surnameTwo: ""
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Carla",
+        surnames: {
+            surnameOne: "Luppi",
+            surnameTwo: ""
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Cristian Damaso",
+        surnames: {
+            surnameOne: "Calderon",
+            surnameTwo: "Rojas"
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Eberth Isaac",
+        surnames: {
+            surnameOne: "Castro",
+            surnameTwo: "Campos"
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Eduard",
+        surnames: {
+            surnameOne: "",
+            surnameTwo: ""
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Erick Andres",
+        surnames: {
+            surnameOne: "Crespin",
+            surnameTwo: "Ponce"
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Federico Carlos",
+        surnames: {
+            surnameOne: "Gatti",
+            surnameTwo: "Teijeiro"
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Francisco",
+        surnames: {
+            surnameOne: "Aguirre",
+            surnameTwo: ""
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Gary",
+        surnames: {
+            surnameOne: "Huayhuasi",
+            surnameTwo: "Lima"
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Guillem",
+        surnames: {
+            surnameOne: "Martínez",
+            surnameTwo: ""
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Ingrid",
+        surnames: {
+            surnameOne: "Barrachina",
+            surnameTwo: ""
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Isabel",
+        surnames: {
+            surnameOne: "Santos",
+            surnameTwo: "Castro"
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Jerry",
+        surnames: {
+            surnameOne: "Aiyaniyo",
+            surnameTwo: ""
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Jessica Alexandra",
+        surnames: {
+            surnameOne: "Jaramillo",
+            surnameTwo: "Moncada"
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Judit",
+        surnames: {
+            surnameOne: "Calvet",
+            surnameTwo: ""
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Lidia",
+        surnames: {
+            surnameOne: "Luque",
+            surnameTwo: ""
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Mamadou",
+        surnames: {
+            surnameOne: "",
+            surnameTwo: ""
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Mario Alberto",
+        surnames: {
+            surnameOne: "Valdés",
+            surnameTwo: "Centurión"
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Mateo",
+        surnames: {
+            surnameOne: "Sandoval",
+            surnameTwo: ""
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Oksana",
+        surnames: {
+            surnameOne: "Fedyukova",
+            surnameTwo: ""
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Rocío",
+        surnames: {
+            surnameOne: "Cejudo",
+            surnameTwo: "Soronellas"
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    },
+    {
+        name: "Rodrigo",
+        surnames: {
+            surnameOne: "Fernández",
+            surnameTwo: ""
+        },
+        age: 28,
+        born: "1994 - ?",
+        killed: false
+    }
+]
