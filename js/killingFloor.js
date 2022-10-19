089b5f15ef1d21fb67cd8374ef5eb3d508cba55e
Kuantize Strigoi <adrian.sunye@gmail.com>
2022-10-19 21:09:48 +0200

    added fog animation, fog out animation and fog horn sound also added responsivness to start view

--- js/killingFloor.js
+++ js/killingFloor.js
@@ -10,21 +10,52 @@ let elementToAppend;
 let classArray = [];
 let elementArray = [];
 
+let audioFogHorn = new Audio('../media/audio/fogHorn.mp3');
+
 /* On load functions
 ========================================================= */
 window.onload = (event) => {
     console.log('Page is fully loaded');
 
+    audioFogHorn.play();
+    audioFogHorn.loop = true; 
+
+    elementToAppend = searchEl('main');
+    elementToAppend.addEventListener("click", handleClick(elementToAppend));
+
     elementToCreate = createEl("div");
-    elementToAppend = searchEl('#mainContainer');
+    elementToAppend = searchEl('#startPoint');
     addNode(elementToAppend, elementToCreate, ["container-md"]);
 
-    elementToCreate = createEl("button");
-    elementToAppend = searchEl('#mainContainer div');
-    addNode(elementToAppend, elementToCreate, ["btn", "btn-danger"]);
+    elementToCreate = createEl("div");
+    elementToAppend = searchEl('#startPoint div');
+    addNode(elementToAppend, elementToCreate, []);
 
+    elementToCreate = createEl("h1");
+    elementToAppend = searchEl('#startPoint div div');
+    addNode(elementToAppend, elementToCreate, ["text-dark"]);
+    elementToAppend.lastElementChild.innerHTML = "DUCK OR GOOSE";
+
+    elementToCreate = createEl("div");
+    elementToAppend = searchEl('#startPoint div');
+    addNode(elementToAppend, elementToCreate, ["d-grid", "gap-2", "col-md-8" , "mx-auto"]);
+
+    elementToCreate = createEl("button");
+    elementToAppend = searchEl('#startPoint div div:last-of-type');
+    addNode(elementToAppend, elementToCreate, ["btn", "btn-orange", "btn-lg"]);
+    setButton(elementToAppend.lastElementChild, "btn-startKilling", "button", "Start Killing");
+
+    elementToCreate = createEl("button");
+    addNode(elementToAppend, elementToCreate, ["btn", "btn-purple", "btn-lg" ]);
+    setButton(elementToAppend.lastElementChild, "btn-addSacrifices", "button", "Add Sacrifices");
+
+    elementToCreate = createEl("div");
+    addNode(elementToAppend, elementToCreate, ["mx-auto"]);
     
-    setButton(elementToAppend.firstElementChild, "btn-startKilling", "button", "Start Killing");
+    elementToCreate = createEl("p");
+    elementToAppend = searchEl('#startPoint div div:last-of-type div');
+    addNode(elementToAppend, elementToCreate, ["text-color-moon"]);
+    elementToAppend.lastElementChild.innerHTML = "Coders still Alive";
 
     addClasses();
 };
@@ -33,9 +64,20 @@ window.onload = (event) => {
 ========================================================= */
 const handleClick = (element) => { // higher order function
     switch (element.id){
+        case "mainContainer":
+            return () => {  
+                elementToAppend = searchEl('#mainContainer .wrapper-2');
+                audioFogHorn.pause();
+                elementToAppend.classList.add("puff-out-hor");
+                setTimeout(() => {
+                    elementToAppend.remove();
+                }, 1000);
+                
+            };
+
         case "btn-startKilling":
             return () => {  
-                removeContent(searchEl("#mainContainer"))  ;   
+                removeContent(searchEl("#startPoint"))  ;   
                 startKilling();                
             };
             
@@ -50,24 +92,24 @@ const handleClick = (element) => { // higher order function
 //function for starting game 
 const startKilling = () => {
     elementToCreate = createEl("div");
-    elementToAppend = searchEl('#mainContainer');
+    elementToAppend = searchEl('#startPoint');
     addNode(elementToAppend, elementToCreate, ["container-md"]);
 
     elementToCreate = createEl("div");
-    elementToAppend = searchEl("#mainContainer div");
+    elementToAppend = searchEl("#startPoint div");
     addNode(elementToAppend, elementToCreate, ["row"]);
 
     elementToCreate = createEl("div");
-    elementToAppend = searchEl("#mainContainer div div");
+    elementToAppend = searchEl("#startPoint div div");
     addNode(elementToAppend, elementToCreate, ["col-md-6"])
     elementToCreate = createEl("div");
     addNode(elementToAppend, elementToCreate, ["col-md-6"])
 
     elementToCreate = createEl("ul");
-    elementToAppend = searchEl("#mainContainer div div div:first-of-type");
+    elementToAppend = searchEl("#startPoint div div div:first-of-type");
     addNode(elementToAppend, elementToCreate, ["list-group", "list-group-flush"]);
 
-    elementToAppend = searchEl("#mainContainer div div div:first-of-type ul");
+    elementToAppend = searchEl("#startPoint div div div:first-of-type ul");
     sacrifices.forEach(el => {
         elementToCreate = createEl("li");
         addNode(elementToAppend, elementToCreate, ["list-group-item"])
@@ -75,7 +117,7 @@ const startKilling = () => {
     });
 
     elementToCreate = createEl("button");
-    elementToAppend = searchEl("#mainContainer div div div:last-of-type");
+    elementToAppend = searchEl("#startPoint div div div:last-of-type");
     addNode(elementToAppend, elementToCreate, ["btn", "btn-danger"]);
 
     setButton(elementToAppend.firstElementChild  , "btn-killSomeone", "button", "Kill Someone");
@@ -130,7 +172,6 @@ const addNode = (eta, etc, stringClass) => {
 //function to add all stored classes
 const addClasses = () => {
     for (let i = 0; i < elementArray.length; i++) {
-        console.log(elementArray)
         elementArray[i].classList.add(classArray[i]);
     }
     elementArray.length = 0;
@@ -154,7 +195,7 @@ const removeContent = (el) => {
 
 //function to cross names in list
 const crossDead = (obj) => {
-    let liElements = document.querySelectorAll("#mainContainer li");
+    let liElements = document.querySelectorAll("#startPoint li");
     console.log(liElements)
     liElements.forEach(el => {
         if(el.innerHTML == `${obj.name} ${obj.surnames.surnameOne}`){
