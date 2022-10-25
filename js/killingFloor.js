739b8f6219618373e210ff71d9a8a0588f9b6a60
AleMCuitino <you@example.com>
2022-10-25 15:44:17 +0200

    Add sounds ducks

--- js/killingFloor.js
+++ js/killingFloor.js
@@ -193,10 +193,19 @@ function callback(event) {
 //function to kill a coder at random
 const killSomeone = () => {
     let movements = Math.floor(Math.random() * (7 - 2) + 2);
-
+    let positionSound 
     for (let i = 0; i < movements; i++) {
-        owl.trigger('next.owl.carousel')
-    }
+        owl.trigger('next.owl.carousel');
+        positionSound = Math.floor(Math.random() * (24 - 2) + 2);
+        let duckSound = []
+        for (let j = 0; j < positionSound; j++) {
+            duckSound[j]  = new Audio(`../media/audio/pato${positionSound}.mp3`);
+            console.log (duckSound[j]);
+            setTimeout (() => {
+                duckSound[j].play()
+            }, 5000);
+        };
+        }
 
     let coderToKill = searchEl("#owlCarousel .active div");
     console.log(coderToKill.id)
